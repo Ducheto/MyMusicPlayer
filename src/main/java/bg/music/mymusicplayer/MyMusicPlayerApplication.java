c59d78efed9c97e21d7905cf0137b48f62831552
Ducheto <75548321+Ducheto@users.noreply.github.com>
2022-08-05 22:09:16 +0300

    Initial commit

---
+++ src/main/java/bg/music/mymusicplayer/MyMusicPlayerApplication.java
@@ -0,0 +1,13 @@
+package bg.music.mymusicplayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyMusicPlayerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyMusicPlayerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/bg/music/mymusicplayer/MyMusicPlayerApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.music.mymusicplayer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyMusicPlayerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
